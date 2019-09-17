88e7cd52c85168cdcd7835555df2d859a8509575
jbeatty88 <gab[8I2(]ErYL!g>
2019-09-17 14:50:45 -0600

    Implemented all of the editDistances but not yet recursive

--- src/JBTests/SpellCorrectorTest.java
+++ src/JBTests/SpellCorrectorTest.java
@@ -3,13 +3,41 @@ package JBTests;
 import org.junit.jupiter.api.Test;
 import spell.SpellCorrector;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
 
 class SpellCorrectorTest {
+    SpellCorrector corrector = new SpellCorrector();
 
     @Test
     void deletionDistance() {
-        SpellCorrector corrector = new SpellCorrector();
-        corrector.deletionDistance("bird", 1);
+        corrector.deletionDistance("bird");
+    }
+
+    @Test
+    void transpositionDistance() {
+        corrector.transpositionDistance("house");
+    }
+
+    @Test
+    void alterationDistance() {
+        corrector.alterationDistance("top");
+    }
+
+    @Test
+    void insertionDistance() {
+        corrector.insertionDistance("askg");
+    }
+
+    @Test
+    void testSuggest() throws IOException {
+        corrector.useDictionary("words.txt");
+//        System.out.println(corrector.trie.toString());
+//        Set<String> wordSet = corrector.suggestedWords("floabt");
+//        for(String s : wordSet) System.out.println(s);
+
+        String suggestion = corrector.suggestSimilarWord("floabt");
+//        System.out.printf("Suggested word: %s", suggestion);
     }
 }
--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -4,9 +4,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class SpellCorrector implements ISpellCorrector {
-    Trie trie = new Trie();
+    public Trie trie = new Trie();
+    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
         try(Scanner sc = new Scanner(new FileReader(dictionaryFileName))) {
@@ -16,34 +19,93 @@ public class SpellCorrector implements ISpellCorrector {
 
     @Override
     public String suggestSimilarWord(String inputWord) {
-        if(trie.find(inputWord).getValue() > 0) return inputWord.toLowerCase();
-        return null;
+        Set<String> matches = new TreeSet<>();
+        String word = inputWord.toLowerCase();
+        if(trie.find(word) != null) return word;
+        else {
+            for(String s : suggestedWords(word)) {
+                if(trie.find(s) != null) matches.add(s);
+            }
+            System.out.println(matches.size());
+        }
+        return " ";
     }
 
-    public ArrayList<String> deletionDistance(String word, int editDist) {
+    public ArrayList<String> deletionDistance(String word) {
         // deletion distance of 1 from “bird” are “ird”, “brd”, “bid”, and “bir”
         ArrayList<String> deletionWords = new ArrayList<>();
-        String subStr1, subStr2, deletionWord;
+        Set<String> generateStrings = new TreeSet<>();
+        String frontSubStr, backSubStr, frontBack;
         for(int i = 0; i < word.length(); i++) {
-            subStr1 = word.substring(i);
-//            subStr2 = word.substring(i, word.length());
-            deletionWord = subStr1;
-            deletionWords.add(deletionWord);
+            frontSubStr = word.substring(0, i);
+            backSubStr = "";
+            if(i + 1 < word.length()) backSubStr = word.substring(i + 1);
+            frontBack = frontSubStr + backSubStr;
+            generateStrings.add(frontSubStr); generateStrings.add(backSubStr); generateStrings.add(frontBack);
         }
+        for(String s : generateStrings) if(s.length() == word.length() - 1) deletionWords.add(s);
 
-        for(String s : deletionWords) System.out.println(s);
         return deletionWords;
     }
 
-    public String[] transpositionDistance(int editDist) {
-        return null;
+    public ArrayList<String> transpositionDistance(String word) {
+        ArrayList<String> transpositionWords = new ArrayList<>();
+        char[] wordCharArray = word.toCharArray();
+        char curLetter, nextLetter;
+        for(int i = 0; i < wordCharArray.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            curLetter = wordCharArray[i];
+            if(i + 1 < wordCharArray.length) {
+                nextLetter = wordCharArray[i + 1];
+                wordCharArray[i] = nextLetter;
+                wordCharArray[i+1] = curLetter;
+                for(char c : wordCharArray) sb.append(c);
+                transpositionWords.add(sb.toString());
+            }
+            wordCharArray = word.toCharArray();
+        }
+        return transpositionWords;
     }
 
-    public String[] alterationDistance(int editDist) {
-        return null;
+    public ArrayList<String> alterationDistance(String word) {
+        char[] wordCharArray = word.toCharArray();
+        ArrayList<String> alterationWords = new ArrayList<>();
+        for(int i = 0; i < word.length(); i++) {
+            for(char c : alphabet) {
+                StringBuilder sb = new StringBuilder();
+                wordCharArray[i] = c;
+                for(char ch : wordCharArray) sb.append(ch);
+                alterationWords.add(sb.toString());
+            }
+            wordCharArray = word.toCharArray();
+        }
+        return alterationWords;
     }
 
-    public String[] insertionDistance(int editDist) {
-        return null;
+    public ArrayList<String> insertionDistance(String word) {
+        ArrayList<String> insertionWords = new ArrayList<>();
+
+        for(int i = 0; i < word.length() + 1; i++) {
+            for(char letter : alphabet) {
+                StringBuilder sb = new StringBuilder(word);
+                sb.insert(i, letter);
+                insertionWords.add(sb.toString());
+            }
+        }
+        return insertionWords;
+    }
+
+    public Set<String> suggestedWords(String word) {
+        ArrayList<String> deletionWords = deletionDistance(word);
+        ArrayList<String> insertionWords = insertionDistance(word);
+        ArrayList<String> alterationWords = alterationDistance(word);
+        ArrayList<String> transpositionWords = transpositionDistance(word);
+        Set<String> suggestedWordSet = new TreeSet<>();
+
+        suggestedWordSet.addAll(deletionWords);
+        suggestedWordSet.addAll(insertionWords);
+        suggestedWordSet.addAll(alterationWords);
+        suggestedWordSet.addAll(transpositionWords);
+        return suggestedWordSet;
     }
 }
